b435907e7bb1459d97f918a9bbb0b1224649a8ec
Yana <126599184+emeli-ya@users.noreply.github.com>
2023-03-06 19:37:23 +0400

    Create task 5.sql

---
+++ lesson 5 - aggregation/task 5.sql
@@ -0,0 +1,9 @@
+/*
+Посчитайте количество курьеров женского пола в таблице couriers. 
+Полученный столбец назовите couriers_count.
+Поле в результирующей таблице: couriers_count
+*/
+
+SELECT COUNT(courier_id) AS couriers_count 
+FROM couriers 
+WHERE sex = 'female'
