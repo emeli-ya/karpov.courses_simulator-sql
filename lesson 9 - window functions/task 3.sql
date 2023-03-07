043105b051dea168b6b51e5c232037574681f7df
Yana <126599184+emeli-ya@users.noreply.github.com>
2023-03-07 16:28:24 +0400

    Create task 3.sql

---
+++ lesson 9 - window functions/task 3.sql
@@ -0,0 +1,16 @@
+/*
+Примените две оконные функции к таблице products для вычисления максимальной и минимальной цены. 
+Для двух окон задайте инструкцию ORDER BY по убыванию цены. Поместите результат вычислений в две колонки max_price и min_price.
+Выведите всю информацию о товарах, включая значения в новых колонках. 
+Результат отсортируйте сначала по убыванию цены товара, затем по возрастанию id товара.
+
+Поля в результирующей таблице: product_id, name, price, max_price, min_price
+*/
+
+SELECT product_id,
+       name,
+       price,
+       max(price) OVER(ORDER BY price desc) max_price,
+       min(price) OVER(ORDER BY price desc) min_price
+FROM   products
+ORDER BY price desc, product_id
