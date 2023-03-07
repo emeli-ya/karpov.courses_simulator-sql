69e09ded5708ebcfb96f0c5d160eef08df329cf3
Yana <126599184+emeli-ya@users.noreply.github.com>
2023-03-07 14:27:34 +0400

    Create task 5.sql

---
+++ lesson 6 - grouping/task 5.sql
@@ -0,0 +1,12 @@
+/*
+Используя функцию DATE_TRUNC, посчитайте, сколько заказов было сделано и сколько было отменено в каждом месяце. 
+Колонку с усечённой датой назовите month, колонку с количеством заказов — orders_count. 
+Результат отсортируйте сначала по месяцам — по возрастанию, затем по типу действия — тоже по возрастанию.
+
+Поля в результирующей таблице: month, action, orders_count
+*/
+
+SELECT DATE_TRUNC('month', time) AS month, action, COUNT(order_id) AS orders_count 
+FROM user_actions 
+GROUP BY month, action 
+ORDER BY month, action
