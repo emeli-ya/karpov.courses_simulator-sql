c86d4da163b42cec0fd6f34ec1cb2bc05a1f8941
Yana <126599184+emeli-ya@users.noreply.github.com>
2023-03-07 16:03:56 +0400

    Create task 6.sql

---
+++ lesson 8 - join/task 6.sql
@@ -0,0 +1,15 @@
+/*
+С помощью FULL JOIN объедините по ключу birth_date таблицы, полученные в результате вышеуказанных запросов (то есть объедините друг с другом два подзапроса). 
+Не нужно изменять их, просто добавьте нужный join. В результат включите две колонки с birth_date из обеих таблиц. 
+Эти две колонки назовите соответственно users_birth_date и couriers_birth_date. 
+Также включите в результат колонки с числом пользователей и курьеров — users_count и couriers_count. 
+Отсортируйте получившуюся таблицу сначала по колонке users_birth_date по возрастанию, затем по колонке couriers_birth_date — тоже по возрастанию.
+
+Поля в результирующей таблице: users_birth_date, users_count,  couriers_birth_date, couriers_count
+*/
+
+SELECT a.birth_date AS users_birth_date, users_count, b.birth_date AS couriers_birth_date, couriers_count 
+FROM ( SELECT birth_date, COUNT(user_id) AS users_count FROM users WHERE birth_date IS NOT NULL GROUP BY birth_date ) a 
+FULL JOIN ( SELECT birth_date, COUNT(courier_id) AS couriers_count FROM couriers WHERE birth_date IS NOT NULL GROUP BY birth_date ) b 
+USING(birth_date) 
+ORDER BY users_birth_date, couriers_birth_date
