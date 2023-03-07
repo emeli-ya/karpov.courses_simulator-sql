758994c3a0fd74f5c0a09b323cf188df6a8411a2
Yana <126599184+emeli-ya@users.noreply.github.com>
2023-03-07 15:53:56 +0400

    Create task 14.sql

---
+++ lesson 7 - subqueries/task 14.sql
@@ -0,0 +1,13 @@
+/*
+Из таблицы orders выведите id и содержимое заказов, которые включают хотя бы один из пяти самых дорогих товаров, доступных в нашем сервисе. 
+Результат отсортируйте по возрастанию id заказа.
+
+Поля в результирующей таблице: order_id, product_ids
+*/
+
+WITH top_products AS ( SELECT product_id FROM products ORDER BY price DESC LIMIT 5 ), 
+unnest AS ( SELECT order_id, product_ids, unnest(product_ids) AS product_id FROM orders ) 
+SELECT DISTINCT order_id, product_ids 
+FROM unnest 
+WHERE product_id IN (SELECT * FROM top_products) 
+ORDER BY order_id
